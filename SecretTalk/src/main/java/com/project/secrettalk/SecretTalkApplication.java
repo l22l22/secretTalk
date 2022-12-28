4809097c6ca48cfb01d41ff097954e2edfe5d102
HyeonMa <HyeonMa@14.38.26.243>
2022-12-28 21:21:19 +0900

    google 연동로그인

--- SecretTalk/src/main/java/com/project/secrettalk/SecretTalkApplication.java
+++ SecretTalk/src/main/java/com/project/secrettalk/SecretTalkApplication.java
@@ -2,7 +2,9 @@ package com.project.secrettalk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SecretTalkApplication {
 
